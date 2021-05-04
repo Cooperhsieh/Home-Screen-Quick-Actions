50a1e55040133fbc4bce96bf737aa6bcccad84ac
Cooperhsieh <cooper79216@gmail.com>
2021-05-04 11:21:02 +0800

    final version

---
+++ Quick Actions/SecondHomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondHomeViewController.swift
+//  Quick Actions
+//
+//  Created by Cooper on 2021/5/3.
+//
+
+import UIKit
+
+class SecondHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Second Home"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
